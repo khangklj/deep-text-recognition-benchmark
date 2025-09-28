d4d89c53899b62287e0cc6044a70d1e7989cd3ae
Khang <khangklj@gmail.com>
2025-09-28 14:30:57 +0700

    adjust create_lmdb with smaller map size

--- create_lmdb_dataset.py
+++ create_lmdb_dataset.py
@@ -24,69 +24,68 @@ def writeCache(env, cache):
 
 
 def createDataset(inputPath, gtFile, outputPath, checkValid=True):
-    """
-    Create LMDB dataset for training and evaluation.
-    ARGS:
-        inputPath  : input folder path where starts imagePath
-        outputPath : LMDB output path
-        gtFile     : list of image path and label
-        checkValid : if true, check the validity of every image
-    """
     os.makedirs(outputPath, exist_ok=True)
-    env = lmdb.open(outputPath, map_size=1073741824) #TODO Changed map size
+
+    # start with a small map size (e.g., 256MB)
+    env = lmdb.open(outputPath, map_size=256 * 1024 * 1024)
+
     cache = {}
     cnt = 1
 
-    with open(gtFile, 'r', encoding='utf-8') as data:
+    with open(gtFile, "r", encoding="utf-8") as data:
         datalist = data.readlines()
 
     nSamples = len(datalist)
     print(nSamples)
-    for i in range(nSamples):
-        #TODO changed the way imagePath is found as well to match my usecase
-        # imagePath, label = datalist[i].strip('\n').split('.jpg')
-        # imagePath += '.jpg'
-        imagePath, label = datalist[i].strip('\n').split(' ', 1)
-        # imagePath += '.jpg'
 
-        # imagePath, label = datalist[i].strip('\n').split('\t')
+    for i in range(nSamples):
+        imagePath, label = datalist[i].strip("\n").split(" ", 1)
         imagePath = os.path.join(inputPath, imagePath)
 
-        # # only use alphanumeric data
-        # if re.search('[^a-zA-Z0-9]', label):
-        #     continue
-
         if not os.path.exists(imagePath):
-            print('%s does not exist' % imagePath)
+            print("%s does not exist" % imagePath)
             continue
-        with open(imagePath, 'rb') as f:
+
+        with open(imagePath, "rb") as f:
             imageBin = f.read()
+
         if checkValid:
             try:
                 if not checkImageIsValid(imageBin):
-                    print('%s is not a valid image' % imagePath)
+                    print("%s is not a valid image" % imagePath)
                     continue
             except:
-                print('error occured', i)
-                with open(outputPath + '/error_image_log.txt', 'a') as log:
-                    log.write('%s-th image data occured error\n' % str(i))
+                print("error occured", i)
+                with open(outputPath + "/error_image_log.txt", "a") as log:
+                    log.write("%s-th image data occured error\n" % str(i))
                 continue
 
-        imageKey = 'image-%09d'.encode() % cnt
-        labelKey = 'label-%09d'.encode() % cnt
+        imageKey = "image-%09d".encode() % cnt
+        labelKey = "label-%09d".encode() % cnt
         cache[imageKey] = imageBin
         cache[labelKey] = label.encode()
 
         if cnt % 1000 == 0:
-            writeCache(env, cache)
+            try:
+                writeCache(env, cache)
+            except lmdb.MapFullError:
+                # if out of space, double the map size
+                curr_limit = env.info()["map_size"]
+                new_limit = curr_limit * 2
+                env.set_mapsize(new_limit)
+                print(f"Resized map_size to {new_limit/1024/1024:.2f} MB")
+                writeCache(env, cache)
+
             cache = {}
-            print('Written %d / %d' % (cnt, nSamples))
+            print("Written %d / %d" % (cnt, nSamples))
+
         cnt += 1
-    nSamples = cnt-1
-    cache['num-samples'.encode()] = str(nSamples).encode()
+
+    nSamples = cnt - 1
+    cache["num-samples".encode()] = str(nSamples).encode()
     writeCache(env, cache)
-    print('Created dataset with %d samples' % nSamples)
+    print("Created dataset with %d samples" % nSamples)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fire.Fire(createDataset)
